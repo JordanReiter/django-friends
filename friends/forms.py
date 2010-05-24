4afc9727765cfc29b19bbfb3448d9e1e617c9ea8
jordan <jordan@192.168.1.3>
2010-05-24 18:49:51 -0400

    asdlfkjdklsfasdflkahsdlfiuha

--- friends/forms.py
+++ friends/forms.py
@@ -88,7 +88,7 @@ class MultiEmailField(forms.CharField):
         # Return an empty list if no input was given.
         if not value:
             return []
-        value = re.sub(r'([A-Z0-9._%+-]+@[A-Z0-9.-]+\.([A-Z]{2,4}|museum))',r'\n\n one=\1;two=\2\n\n',value,re.IGNORECASE)
+        value = re.sub(r'([A-Z]+)',r'\n\n one=\1;two=\2\n\n',value,re.IGNORECASE)
         return value
 #        return re.split(r'[\s;,]+',value)
 
