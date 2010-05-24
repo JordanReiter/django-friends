d7c62fc80d904aaa8a45b4a238266d7411b3b263
jordan <jordan@192.168.1.3>
2010-05-24 19:09:21 -0400

    JUST KEEEP GOING

--- friends/forms.py
+++ friends/forms.py
@@ -89,6 +89,10 @@ class MultiEmailField(forms.CharField):
         if not value:
             return []
         value = re.sub(r'[;,\r\n\t]+?\s*("?[^<@>]+?"?\s)?<?([A-Za-z0-9._%+-]+?@[A-Za-z0-9.-]+?\.([A-Za-z]{2,4}|museum))>?',r'\n\2\n',str(", %s ," % value),re.IGNORECASE)
+        value = re.sub(r'[;,\r\n\t]+?\s*("?[^<@>]+?"?\s)?<?([A-Za-z0-9._%+-]+?@[A-Za-z0-9.-]+?\.([A-Za-z]{2,4}|museum))>?',r'\n\2\n',str(", %s ," % value),re.IGNORECASE)
+        value = re.sub(r'[;,\r\n\t]+?\s*("?[^<@>]+?"?\s)?<?([A-Za-z0-9._%+-]+?@[A-Za-z0-9.-]+?\.([A-Za-z]{2,4}|museum))>?',r'\n\2\n',str(", %s ," % value),re.IGNORECASE)
+        value = re.sub(r'[;,\r\n\t]+?\s*("?[^<@>]+?"?\s)?<?([A-Za-z0-9._%+-]+?@[A-Za-z0-9.-]+?\.([A-Za-z]{2,4}|museum))>?',r'\n\2\n',str(", %s ," % value),re.IGNORECASE)
+        value = re.sub(r'[;,\r\n\t]+?\s*("?[^<@>]+?"?\s)?<?([A-Za-z0-9._%+-]+?@[A-Za-z0-9.-]+?\.([A-Za-z]{2,4}|museum))>?',r'\n\2\n',str(", %s ," % value),re.IGNORECASE)
         return value
 #        return re.split(r'[\s,;]+',value)
 
