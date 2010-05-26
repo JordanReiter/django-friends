700f9d2f2b50f138a19f15cd69581c86d174ea27
jordan <jordan@192.168.0.129>
2010-05-26 10:49:41 -0400

    debug

--- friends/importer.py
+++ friends/importer.py
@@ -35,7 +35,7 @@ def import_outlook(stream, user):
         import difflib
         differ = difflib.ndiff(stream,g)
         for n in differ:
-            resp += "\n\n%s" % repr(n)
+            resp += "\n\n%s" % n
         raise Exception('<pre><h1>Different</h1>%s' % resp)
     else:
         raise Exception("They're the same")
