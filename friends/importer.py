b76eea23a673e4058bbc50efaeb16026955fb684
jordan <jordan@192.168.0.129>
2010-05-26 12:00:05 -0400

    type is O

--- friends/importer.py
+++ friends/importer.py
@@ -147,7 +147,7 @@ def import_outlook(stream, user):
             try:
                 Contact.objects.get(owner=user, email=contact_vals['email'])
             except Contact.DoesNotExist:
-                Contact(owner=user,type='I',**contact_vals).save()
+                Contact(owner=user,type='O',**contact_vals).save()
                 imported += 1
             except KeyError:
                 raise Exception("Email not found; this is the line:\n%s\n" % line)
