56689b3d01e75df6545456f9792ea51c254904fd
jordan <jordan@192.168.1.3>
2010-06-01 15:46:47 -0400

    Limit to "my contacts"

--- friends/importer.py
+++ friends/importer.py
@@ -266,22 +266,22 @@ def import_google(authsub_token, user):
     contacts_service.SetAuthSubToken(authsub_token)
     contacts_service.UpgradeToSessionToken()
     entries = []
-    query = gdata.service.Query(feed='/m8/feeds/groups/default/full')
-    feed = contacts_service.GetGroupsFeed(query.ToUri())
-    result = ""
-    for i, entry in enumerate(feed.entry):
-        result+= '\n%s %s' % (i+1, entry.title.text)
-        # Display the group id which can be used to query the contacts feed.
-        result+= '        Group ID: %s' % entry.id.text
-        # Display extended properties.
-        for extended_property in entry.extended_property:
-            if extended_property.value:
-                value = extended_property.value
-            else:
-                value = extended_property.GetXmlBlobString()
-            result+= '        Extended Property %s: %s' % (extended_property.name, value)
-    raise Exception("The result is <pre>%s</pre>" % result) 
-    feed = contacts_service.GetContactsFeed()
+#    query = gdata.service.Query(feed='/m8/feeds/groups/default/full')
+#    feed = contacts_service.GetGroupsFeed(query.ToUri())
+#    result = ""
+#    for i, entry in enumerate(feed.entry):
+#        result+= '\n%s %s' % (i+1, entry.title.text)
+#        # Display the group id which can be used to query the contacts feed.
+#        result+= '        Group ID: %s' % entry.id.text
+#        # Display extended properties.
+#        for extended_property in entry.extended_property:
+#            if extended_property.value:
+#                value = extended_property.value
+#            else:
+#                value = extended_property.GetXmlBlobString()
+#            result+= '        Extended Property %s: %s' % (extended_property.name, value)
+#    raise Exception("The result is <pre>%s</pre>" % result) 
+    feed = contacts_service.GetContactsFeed(group="Contacts")
     entries.extend(feed.entry)
     next_link = feed.GetNextLink()
     while next_link:
