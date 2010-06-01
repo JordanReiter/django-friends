40a519795c18abb47180f2a7b4094aa2e49aee5b
jordan <jordan@192.168.1.3>
2010-06-01 18:29:42 -0400

    render_to

--- friends/views.py
+++ friends/views.py
@@ -487,7 +487,8 @@ def edit_friends(request, friend=None, redirect_to='edit_friends', form_class=Fr
             friend_forms.append(form_class(instance=friendship, user=request.user, friend=f['friend'], prefix='friend_%s' % counter))
     return locals(), template_name
 
-
+@render_to()
+@login_required
 def recommended_friends(request, template_name="friends/recommended_friends.html"):
     recommended_friends = FriendSuggestion.objects.filter(user=request.user)
     return locals(), template_name
