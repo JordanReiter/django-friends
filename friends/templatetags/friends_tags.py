ce94dc8e80668a2ab8db0ccf2c2baad355e8cf40
MakeToLearn <jordan@aace.org>
2011-10-17 14:45:58 -0400

    Overwrote function oops

--- friends/templatetags/friends_tags.py
+++ friends/templatetags/friends_tags.py
@@ -1,4 +1,4 @@
-from friends.utils import shared_friends
+from friends.utils import shared_friends as get_shared_friends
 from django import template
 from django.core.urlresolvers import NoReverseMatch
 
@@ -10,6 +10,6 @@ def shared_friends(context):
     return {
         'request': context['request'],
         'friend': friend,
-        'shared_friends': shared_friends(request.user, friend)
+        'shared_friends': get_shared_friends(request.user, friend)
     }
 register.inclusion_tag('friends/shared_friends.inc', takes_context=True)(shared_friends)
